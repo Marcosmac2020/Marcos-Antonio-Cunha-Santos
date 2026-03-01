80fca4d20f019992f7bcf8a11df1131eb8f1cae2
Marcos Antonio Cunha <marcos@marcosantoniocunha.com.br>
2026-03-01 09:19:06 -0300

    Update to final domain marcosantoniocunha.com.br

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,7 @@
-export const API_BASE = 'https://marcosantoniocunhasa1771856246033.0772036.meusitehostgator.com.br/wp-json/pericia-trabalhista-pro/v1';
-export const WP_BASE = 'https://marcosantoniocunhasa1771856246033.0772036.meusitehostgator.com.br';
+// Configuração final - Domínio ativo
+export const API_BASE = 'https://marcosantoniocunha.com.br/wp-json/pericia-trabalhista-pro/v1';
+export const WP_BASE = 'https://marcosantoniocunha.com.br';
+
+// Configuração antiga - Hospedagem temporária (comentada)
+// export const API_BASE = 'https://marcosantoniocunhasa1771856246033.0772036.meusitehostgator.com.br/wp-json/pericia-trabalhista-pro/v1';
+// export const WP_BASE = 'https://marcosantoniocunhasa1771856246033.0772036.meusitehostgator.com.br';
